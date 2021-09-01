195227210aae6ba50b8c24b00cb7de2f6b8e94da
LOTTOCRF6 <sithandathuzipho@gmail.com>
2021-09-01 16:32:36 +0200

    Update2

--- index.js
+++ index.js
@@ -1,5 +1,112 @@
-////////////////////////////////////
-// NAVIGATION SHOW/HIDE
+let products = [];
+let cart = [];
+
+// Show Products
+fetch("https://ancient-lowlands-29535.herokuapp.com/brand-products/")
+  .then((res) => res.json())
+  .then((data) => {
+    products = data;
+    console.log(data);
+    make_products(data);
+  });
+
+// Making Products
+function make_products(products) {
+  let product_container = document.querySelector("#products-container");
+  product_container.innerHTML = "";
+  products.data.forEach((product) => {
+    product_container.innerHTML += `
+        <div class = "products">
+            <img src="${product.image}" class = "product-image"> 
+            <h4 class = "product-title"> ${product.product_tittle}</h4>
+            <p class = "product-description"> ${product.brand_name}</p>
+            <p class = "product-description"> ${product.description}</p>
+            <p class = "product-price">${product.price} </p>
+            <button onclick="addToCart(${product.product_id})">Add to Cart</button>
+            
+        </div>
+    `;
+  });
+}
+
+// Search
+function searchProducts() {
+  let searchTerm = document.querySelector("#searchTerm").value;
+  // console.log(searchTerm);
+
+  let searchedProducts = products.data.filter((product) =>
+    product.product_tittle.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  console.log(searchedProducts);
+  let product_container = document.querySelector("#products-container");
+  product_container.innerHTML = "";
+  products.data.forEach((product) => {
+    // console.log(product);
+    product_container.innerHTML += `
+        <div class = "products">
+            <img src="${product.image}" class = "image"> 
+            <h4 class = "product-title"> ${product.product_tittle}</h4>
+            <p class = "product-description"> ${product.description}</p>
+            <p class = "product-price">${product.price} </p>
+            <button onclick="addToCart(${product.product_id})">Add to Cart</button>
+            
+        </div>
+    `;
+  });
+  // make_products(searchedProducts);
+}
+
+// CARTS
+function toggleCart() {
+  document.querySelector("#cart").classList.toggle("active");
+}
+function renderCart(cartItems) {
+  let cartContainer = document.querySelector("#cart");
+  cartContainer.innerHTML = "";
+  if (cartItems.length > 0) {
+    cartItems.map((cartItem) => {
+      cartContainer.innerHTML += `
+      <div class = "products">
+            <img src="${cartItem.image}" class = "image">
+            <div class = "product-content"> 
+                <h4 class = "product-title"> ${cartItem.product_tittle}</h4>
+                <p class = "product-description"> ${cartItem.description}</p>
+                <p class = "product-price">${cartItem.price} </p>
+                <button class ="revome_cart" onclick="removeItem(${cartItem.product_id})">Remove item</button>
+            </div>
+            
+        </div>
+      
+      
+      `;
+    });
+    let totalPrice = cartItems.reduce((total, item) => total + item.price,0);
+    cartContainer.innerHTML += `<h3> Total is: ${totalPrice} </h3>`;
+  } else {
+    cartContainer.innerHTML = "<h2> No items in cart</h2>";
+  }
+}
+
+function addToCart(id) {
+  console.log(products.data);
+  let product = products.data.find((item) => {
+    return item.id == id;
+  });
+  cart.push(product);
+  renderCart(cart);
+}
+function removeItem(id) {
+  let product = products.data.find((item) => {
+    return item.id == id;
+  });
+  //console.log(product);
+
+  cart.splice(
+    cart.findIndex((a) => a.id === product.id),
+    1
+  );
+  renderCart(cart);
+}
 
 $("nav ul").hide();
 
@@ -61,3 +168,4 @@ $( document ).ready( function() {
 $( window ).resize( function() {
   centerSplash();
 });
+
---
+++ user-register.js
@@ -0,0 +1,29 @@
+mystorage = window.localStorage;
+function userRegister() {
+  fetch("https://ancient-lowlands-29535.herokuapp.com/user-registration/", {
+    method: "POST",
+    body: JSON.stringify({
+      first_name: document.getElementById("first_name").value,
+      last_name: document.getElementById("last_name").value,
+      username: document.getElementById("username").value,
+      password: document.getElementById("password").value,
+      address: document.getElementById("address").value,
+      phone: document.getElementById("phone").value,
+      email: document.getElementById("email").value,
+    }),
+    headers: {
+      "Content-type": "application/json",
+      Authorization: `jwt ${mystorage.getItem("jwt-token")}`,
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      if (data["message"] == "success") {
+        alert("Registered successfully, please log in.");
+        window.location.href = "./user_login";
+      } else {
+        alert("Please enter correct information");
+      }
+    });
+}
